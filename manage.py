33dbf65cfcfd8142649d44c2070c3e37cb207166
Kirk Gleason <kgleason@bloommg.com>
2015-03-20 21:06:21 -0500

    Manage can now run without a database

--- manage.py
+++ manage.py
@@ -7,8 +7,8 @@ manager.add_command('db', MigrateCommand)
 
 app.debug = True
 
-if not TwilioConfig.query.all():
-    print "There is no twilio config in the database. Please add your account_sid and auth_token into the database."
-    exit()
+#if not TwilioConfig.query.all():
+#    print "There is no twilio config in the database. Please add your account_sid and auth_token into the database."
+#    exit()
 
 manager.run()
