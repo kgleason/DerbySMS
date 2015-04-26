229d50032255149ab1c7da3c9b9cb765c1c4dc47
Kirk Gleason <kgleason@gmail.com>
2015-04-26 16:09:10 -0400

    custom javascript for handling the socketio connections.

---
+++ DerbySMS/static/js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  var socket = io.connect();
+
+  // the socket.io documentation recommends sending an explicit package upon connection
+  socket.on('connect', function() {
+    socket.emit('connect', {data: 'I\'m connected!'});
+    });
+
+  socket.on('update value', function(msg) {
+    console.log(msg);
+    //console.log('About to change the value of ' + msg.who + ' from ' + $('input#'+msg.who).is(':checked') + ' to ' + msg.data);
+    //$('input#'+msg.who).prop("checked",(msg.data));
+  });
+
+  socket.on('update bet', function(msg) {
+    console.log(msg);
+    var data = JSON.parse(msg);
+    // The JSON should look something like this:
+    //{'horse_id : 1, 'bettor' : 1, 'amount' : 1, 'ago' : 'Just Now'}
+
+    $('td#person'+data.horse_id).html(data.bettor);
+    $('td#bet'+data.horse_id).html(data.amount);
+    $('td#ago'+data.horse_id).html(data.ago);
+  });
+
+  socket.on('insert row', function(msg) {
+    var data = JSON.parse(msg)
+    var $html = '<tr>\
+      <td>' + data.horse_name + '(' + data.horse_nickname + ')</td>\
+      <td>No one</td>\
+      <td>$0</td>\
+      <td>Never</td></tr>'
+    $('#curBetsTable TBODY').append($html)
+  });
+
+});
